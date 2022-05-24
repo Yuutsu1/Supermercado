488b96fc217e20ad423962112b0a4bb46ec3e4fc
Jordy <jordyfmm@gmail.com>
2022-05-23 20:12:03 -0500

    Refactoring

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,5 @@ public class Main {
     public static void main(String[] args) {
         Ventana ventana = new Ventana();
         ventana.iniciarSesion();
-		System.out.println("Probando :)");
     }
 }
--- src/com/company/PanelPedido.java
+++ src/com/company/PanelPedido.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class PanelPedido {
+public class PanelPedido extends Panel{
 
     public void mostrarFormulario(){
         System.out.println("Mostrando Formulario para realizar un nuevo pedido");
